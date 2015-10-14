9a06d02e2d935e77adb8d449f734bb4819b06986
Amar Tumballi <amarts@gmail.com>
2015-10-14 12:28:25 +0530

    Add dependency of 'library' in Application 2

--- application2/src/main/java/com/example/android/application2/MainActivity.java
+++ application2/src/main/java/com/example/android/application2/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.android.application2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.example.android.library.Greet;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i("Demo", Greet.sayHelloInSpanish());
     }
 }
